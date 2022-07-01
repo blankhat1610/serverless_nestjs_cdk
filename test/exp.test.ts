37ce897f4a4ebff88239162ab39a8f286fedac8d
Blank Hat <maixuanthuong1610@gmail.com>
2022-07-01 16:45:31 +0700

    first commit

---
+++ test/exp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Exp from '../lib/exp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/exp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Exp.ExpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
